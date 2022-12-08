0e88a4cb29ceceb8ea16d52abacb1e3844bcce9d
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:52:31 +0330

    make view model in activity

--- app/src/main/java/com/example/samplekoindependency/MainActivity.kt
+++ app/src/main/java/com/example/samplekoindependency/MainActivity.kt
@@ -7,12 +7,18 @@ import androidx.activity.compose.setContent
 import com.example.samplekoindependency.ui.theme.SampleKoinDependencyTheme
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
+import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.compose.getViewModel
 import org.koin.androidx.compose.viewModel
+import org.koin.androidx.scope.activityScope
+import org.koin.core.scope.Scope
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(),AndroidScopeComponent {
 
     //private val viewModel by viewModels<MainViewModel>()
+
+    override val scope: Scope by activityScope()
+
 
     private val api by inject<MyApi>()
 
