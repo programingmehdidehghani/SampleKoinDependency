51cfb6454e889bc42daf6a72a58d0acd6152a522
mehdi <dehghani_m@rocketmail.com>
2022-12-08 19:22:49 +0330

    make fun view model

--- app/src/main/java/com/example/samplekoindependency/MainViewModel.kt
+++ app/src/main/java/com/example/samplekoindependency/MainViewModel.kt
@@ -3,4 +3,10 @@ package com.example.samplekoindependency
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private val repositoryImpl = MainRepositoryImpl()
+
+    fun doNetworkCall(){
+        
+    }
 }
