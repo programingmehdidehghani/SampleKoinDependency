ff43db102ad4c1280d39749205996cad0076f72d
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:42:57 +0330

    application in manifest

--- app/src/main/java/com/example/samplekoindependency/MyApplication.kt
+++ app/src/main/java/com/example/samplekoindependency/MyApplication.kt
@@ -2,6 +2,7 @@ package com.example.samplekoindependency
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MyApplication : Application() {
@@ -9,6 +10,7 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin{
+            androidLogger()
             androidContext(this@MyApplication)
             modules(appModule)
         }
