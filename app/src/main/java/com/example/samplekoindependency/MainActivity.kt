1da803c7f2814f0a40d78eedd3508670fa75a888
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:32:01 +0330

    make view model in activity

--- app/src/main/java/com/example/samplekoindependency/MainActivity.kt
+++ app/src/main/java/com/example/samplekoindependency/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.samplekoindependency
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,34 +11,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.samplekoindependency.ui.theme.SampleKoinDependencyTheme
+import org.koin.androidx.compose.getViewModel
+import org.koin.androidx.compose.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    //private val viewModel by viewModels<MainViewModel>()
+
+    private val 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SampleKoinDependencyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel = getViewModel<MainViewModel>()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SampleKoinDependencyTheme {
-        Greeting("Android")
-    }
-}
