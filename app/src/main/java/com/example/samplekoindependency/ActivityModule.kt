08438c738e2882a751b7134360e38e2e03748737
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:48:03 +0330

    make module activity

--- app/src/main/java/com/example/samplekoindependency/ActivityModule.kt
+++ app/src/main/java/com/example/samplekoindependency/ActivityModule.kt
@@ -1,5 +1,15 @@
 package com.example.samplekoindependency
 
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
 class ActivityModule {
-    
+
+val activityModule = module {
+    scope<MainActivity>{
+        scoped { "Hello" }
+    }
+}
+
+
 }
