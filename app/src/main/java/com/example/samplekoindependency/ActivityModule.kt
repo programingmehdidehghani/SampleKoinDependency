c0a7beebf3cf5ee1c28b862bcc275832d8488f17
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:50:27 +0330

    put activitymodule in application class

--- app/src/main/java/com/example/samplekoindependency/ActivityModule.kt
+++ app/src/main/java/com/example/samplekoindependency/ActivityModule.kt
@@ -1,15 +1,9 @@
 package com.example.samplekoindependency
 
-import org.koin.core.module.Module
 import org.koin.dsl.module
-
-class ActivityModule {
 
 val activityModule = module {
     scope<MainActivity>{
         scoped { "Hello" }
     }
-}
-
-
 }
--- app/src/main/java/com/example/samplekoindependency/MyApplication.kt
+++ app/src/main/java/com/example/samplekoindependency/MyApplication.kt
@@ -12,7 +12,7 @@ class MyApplication : Application() {
         startKoin{
             androidLogger()
             androidContext(this@MyApplication)
-            modules(appModule)
+            modules(appModule,activityModule)
         }
     }
 }
