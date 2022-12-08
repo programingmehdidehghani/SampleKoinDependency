f9462c6fa54b5b9b11fa1600e81f1c9fa1ac7d98
mehdi <dehghani_m@rocketmail.com>
2022-12-08 22:01:38 +0330

    make instance view model

--- app/src/main/java/com/example/samplekoindependency/MainActivity.kt
+++ app/src/main/java/com/example/samplekoindependency/MainActivity.kt
@@ -10,24 +10,26 @@ import org.koin.android.ext.android.inject
 import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.compose.getViewModel
 import org.koin.androidx.compose.viewModel
+import org.koin.androidx.scope.activityRetainedScope
 import org.koin.androidx.scope.activityScope
 import org.koin.core.scope.Scope
 
 class MainActivity : ComponentActivity(),AndroidScopeComponent {
 
-    //private val viewModel by viewModels<MainViewModel>()
 
     override val scope: Scope by activityScope()
+    private val hello by inject<String>()
 
 
     private val api by inject<MyApi>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println(hello)
         setContent {
             SampleKoinDependencyTheme {
-                val viewModel = getViewModel<MainViewModel>()
-                viewModel.doNetworkCall()
+                /*val viewModel = getViewModel<MainViewModel>()
+                viewModel.doNetworkCall()*/
             }
         }
     }
