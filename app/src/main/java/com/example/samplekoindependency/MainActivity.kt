ad8a298610781e1ce34d3613467e1c96f4fa9ba4
mehdi <dehghani_m@rocketmail.com>
2022-12-08 21:36:03 +0330

    continue koin

--- app/src/main/java/com/example/samplekoindependency/MainActivity.kt
+++ app/src/main/java/com/example/samplekoindependency/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.samplekoindependency
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
+
 import com.example.samplekoindependency.ui.theme.SampleKoinDependencyTheme
+import org.koin.android.ext.android.get
+import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.getViewModel
 import org.koin.androidx.compose.viewModel
 
@@ -20,7 +14,7 @@ class MainActivity : ComponentActivity() {
 
     //private val viewModel by viewModels<MainViewModel>()
 
-    private val 
+    private val api by inject<MyApi>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
